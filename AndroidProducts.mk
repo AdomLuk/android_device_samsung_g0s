9bb0c36c6ee9f718e7cf8ea9b64cc16ea97ede46
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-05-12 16:28:24 +0200

    g0s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-S906B Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_g0s.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_g0s-user \
+    omni_g0s-userdebug \
+    omni_g0s-eng
---
+++ omni_g0s.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from g0s device
+$(call inherit-product, device/samsung/g0s/device.mk)
+
+PRODUCT_DEVICE := g0s
+PRODUCT_NAME := omni_g0s
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-S906B
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="g0sxxx-user 12 SP1A.210812.016 S906BXXU9EXE1 release-keys"
+
+BUILD_FINGERPRINT := samsung/g0sxxx/g0s:12/SP1A.210812.016/S906BXXU9EXE1:user/release-keys
